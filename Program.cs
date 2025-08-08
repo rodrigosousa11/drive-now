a3547838da5950afa6a6f7aa7dc4f36244410428
Rodrigo Sousa <rodrigosousa1105@gmail.com>
2025-08-08 17:03:25 +0100

    Minor style changes

--- Program.cs
+++ Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Vehicles}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
---
+++ Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DriveNow.Views.Home
+{
+    public class IndextModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
